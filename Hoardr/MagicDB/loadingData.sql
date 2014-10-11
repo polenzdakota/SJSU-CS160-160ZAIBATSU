6bd6c47ced0b4e14edd779b0dcc5dd4d3d4144d3
Sean <sean.papay@gmail.com>
2014-10-10 22:58:51 -0700

    made paths relative

--- Hoardr/MagicDB/loadingData.sql
+++ Hoardr/MagicDB/loadingData.sql
@@ -5,8 +5,8 @@ DROP TABLE LIBRARY;
 
 SHOW TABLES;
 
-LOAD DATA LOCAL INFILE 'C:\\Users\\Chris\\Documents\\MagicDB\\users.txt' INTO TABLE USERS;
-LOAD DATA LOCAL INFILE 'C:\\Users\\Chris\\Documents\\MagicDB\\samplelibrary.txt' INTO TABLE LIBRARY;
-LOAD DATA LOCAL INFILE 'C:\\Users\\Chris\\Documents\\MagicDB\\collection.txt' INTO TABLE COLLECTION1;
-LOAD DATA LOCAL INFILE 'C:\\Users\\Chris\\Documents\\MagicDB\\deck.txt' INTO TABLE DECK;
-#LOAD DATA LOCAL INFILE 'C:\\Users\\Chris\\Documents\\MagicDB\\collection_link.txt' INTO TABLE COLLECTION_LINK;
+LOAD DATA LOCAL INFILE 'users.txt' INTO TABLE USERS;
+LOAD DATA LOCAL INFILE 'samplelibrary.txt' INTO TABLE LIBRARY;
+LOAD DATA LOCAL INFILE 'collection.txt' INTO TABLE COLLECTION1;
+LOAD DATA LOCAL INFILE 'deck.txt' INTO TABLE DECK;
+#LOAD DATA LOCAL INFILE 'collection_link.txt' INTO TABLE COLLECTION_LINK;
