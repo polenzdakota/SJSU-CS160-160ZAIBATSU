e79152a212401bd351a70b4a3420a568693a6df8
Dakota Polenz <polenzdakota@yahoo.com>
2014-12-07 13:43:13 -0800

    blah

--- website/src/java/Models/Queries.java
+++ website/src/java/Models/Queries.java
@@ -28,9 +28,8 @@ public class Queries {
     Statement statement;
     Connector con;
     public Queries(){
-        this.con = new Connector(); 
+        this.con = new Connector();
         con.setConnection();
-        
     }
     public ResultSet collectionJoin(String username)throws SQLException{
         String sql = "Select * from Cards Inner Join ? on Cards.card_id = ?.card_collection_id";
@@ -101,6 +100,7 @@ public class Queries {
      public String retrievePassword(String username) throws SQLException{
          String pass = "";
          String sql = "Select user_login_password from User where user_login_name = ?";
+         con.setConnection();
          p = con.connection.prepareStatement(sql);
          p.setString(1,username);
          try{
@@ -120,7 +120,6 @@ public class Queries {
        * @throws SQLException 
        */
      public ResultSet searchREGEXP(String s) throws SQLException{
-         con.setConnection();
        try{
            String sql = "SELECT * from Cards where card_name REGEXP ? order by card_name;";
            p = con.connection.prepareStatement(sql);
@@ -133,13 +132,12 @@ public class Queries {
        }catch(SQLException e){
            System.out.println("SQL error" + e.getMessage());
        }
-      
+       
        return rs;
      }
      public ResultSet oneCard(String s) throws SQLException{
-         
-       con = new Connector();
-       con.setConnection();
+ 
+      
        try{
            String sql = "SELECT * from Cards where card_id = ?";
            p = con.connection.prepareStatement(sql);
@@ -155,6 +153,7 @@ public class Queries {
        return rs;
      }
      public void insert(int cardId,String username) throws SQLException{
+         
          String sql = "Insert into ? (card_collection_id) values(?)";
          p = con.connection.prepareStatement(sql);
          p.setString(1, username);
